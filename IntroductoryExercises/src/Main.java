7809fbebdb92f4b99f9fc2fd8032cd213e3bcb97
Cannon Biggs <cebiggs@unomaha.edu>
2015-05-24 15:25:31 -0500

    Add user interface at commandline

--- IntroductoryExercises/src/Main.java
+++ IntroductoryExercises/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Created by cannon on 5/23/15.
@@ -7,46 +8,118 @@ public class Main
 {
     public static void main(String[] args)
     {
-        int n = 8;
-        String name = "Cannon";
+        int n = 0;
+        String name = "";
+        int userInput = 0;
 
         Triangle triangle = new Triangle();
         Diamond diamond = new Diamond();
         FizzBuzz fizzbuzz = new FizzBuzz();
         PrimeFactors primeFactors = new PrimeFactors();
+        Scanner in = new Scanner(System.in);
 
-        triangle.printAsterisk();
-        System.out.println();
+        while ( userInput != 10 ) {
 
-        triangle.drawHorizontalLine( n );
-        System.out.println();
+            System.out.println("Operations:\n1: Print one asterisk");
+            System.out.println("2: Draw a horizontal line");
+            System.out.println("3: Draw a vertical line");
+            System.out.println("4: Draw a right triangle");
+            System.out.println("5: Draw an isosceles triangle");
+            System.out.println("6: Draw a diamond");
+            System.out.println("7: Draw a diamond with name");
+            System.out.println("8: Do the FizzBuzz");
+            System.out.println("9: Find the prime factors of a number");
+            System.out.println("10: Quit\n");
+            System.out.print("Enter a number to perform an operation: ");
 
-        triangle.drawVerticalLine( n );
-        System.out.println();
+            userInput = in.nextInt();
 
-        triangle.drawRightTriangle( n );
-        System.out.println();
+            switch (userInput) {
+                case 1:
+                    triangle.printAsterisk();
+                    break;
+                case 2:
+                    System.out.print("Enter number of asterisks: ");
+                    n = in.nextInt();
+                    if (validate(n)) {
+                        triangle.drawHorizontalLine( n );
+                    }
+                    break;
+                case 3:
+                    System.out.print("Enter number of lines: ");
+                    n = in.nextInt();
+                    if (validate(n)) {
+                        triangle.drawVerticalLine( n );
+                    }
+                    break;
+                case 4:
+                    System.out.print("Enter number of lines for triangle: ");
+                    n = in.nextInt();
+                    if (validate(n)) {
+                        triangle.drawRightTriangle( n );
+                    }
+                    break;
+                case 5:
+                    System.out.print("Enter a number for isosceles triangle: ");
+                    n = in.nextInt();
+                    if (validate(n)) {
+                        diamond.printIsoscelesTriangle(n, 0);
+                    }
+                    break;
+                case 6:
+                    System.out.print("Enter number for diamond: ");
+                    n = in.nextInt();
+                    if (validate(n)) {
+                        diamond.printDiamond(n);
+                    }
+                    break;
+                case 7:
+                    System.out.print("Enter number for diamond: ");
+                    n = in.nextInt();
+                    System.out.print("Enter a name: ");
+                    name = in.next();
+                    if (validate(n)) {
+                        diamond.printDiamondWithName(n, name);
+                    }
+                    break;
+                case 8:
+                    fizzbuzz.FizzBuzz();
+                    break;
+                case 9:
+                    System.out.print("Enter number of which to find prime factors: ");
+                    n = in.nextInt();
 
-        diamond.printIsoscelesTriangle( n, 0 );
-        System.out.println();
+                    if (validate(n)) {
+                        ArrayList<Integer> primeFactorsList = primeFactors.generate( n );
+                        if (primeFactorsList.isEmpty()) {
+                            System.out.println("There are no prime factors.");
+                        }
+                        else {
+                            for ( int factor : primeFactorsList ) {
+                                System.out.printf("%d ", factor);
+                            }
+                        }
+                    }
 
-        diamond.printDiamond( n );
-        System.out.println();
+                    break;
+                case 10:
+                    System.out.println("Bye!");
+                    return;
+                default:
+                    break;
+            }
+            System.out.println();
+            System.out.println();
+        }
+    }
 
-        diamond.printDiamondWithName( n, name );
-        System.out.println();
-
-        fizzbuzz.FizzBuzz();
-        System.out.println();
-
-        ArrayList<Integer> primeFactorsList = primeFactors.generate( 30 );
-        if (primeFactorsList.isEmpty()) {
-            System.out.println("There are no prime factors.");
+    public static boolean validate( int n ) {
+        if ( n >= 0 ) {
+            return true;
         }
         else {
-            for ( int factor : primeFactorsList ) {
-                System.out.printf("%d ", factor);
-            }
+            System.out.println("Sorry, the number must be greater than 0.");
+            return false;
         }
     }
 }
